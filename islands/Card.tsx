1a2aa818eb6befa8ea912d8203a2c1c6f4c01071
Guillaume Robin <gdream.dev@gmail.com>
2024-01-08 21:08:43 +0100

    Rework all website

---
+++ islands/Card.tsx
@@ -0,0 +1,31 @@
+interface CardProps {
+  imgSrc: string;
+  title: string;
+  description: string;
+  tags: string[];
+}
+
+export default function Card(props: CardProps) {
+  return (
+    <a href="#" class="mx-4 my-4">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg background">
+        <img
+          class="w-full"
+          src={props.imgSrc}
+          alt="Sunset in the mountains"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{props.title}</div>
+          <p class="text-white text-base">{props.description}</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          {props.tags.map((tag) => (
+            <span class="inline-block bg-brown text-white rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
+              #{tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </a>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,46 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import Card from "../islands/Card.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <div class="h-screen w-screen">
+      <div class="px-4 pt-5 mx-auto background" style="height:20%;">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <img
+            src="/logo.svg"
+            width="128"
+            alt="the GDream Studio logo: a black and grey background with a red G-letter and a white DREAM text."
+          />
+          <h1 class="text-4xl font-bold mt-4">GDream Studio</h1>
+          <h2 class="text-1xl font-bold mt-2">
+            An independant video game studio
+          </h2>
+        </div>
+      </div>
+
+      <div class="px-4 py-8 mx-auto background-2 overflow-scroll" style="height:70%;">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <p class="text-4xl font-bold">Our games</p>
+
+          <div class="grid grid-flow-row col-auto my-8">
+            <Card
+              title="Coming soon..."
+              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptatibus quia, nulla! Maiores et perferendis eaque,
+      exercitationem praesentium nihil."
+              imgSrc="/card-top.jpg"
+              tags={["puzzle", "3D", "godot", "farming"]}
+            />
+
+          </div>
+        </div>
+      </div>
+
+      <div class="px-4 py-8 mx-auto background-3" style="height:10%;">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <p class="text-xs font-bold">Copyrights 2024 @ GDream Studio</p>
+        </div>
       </div>
     </div>
   );
